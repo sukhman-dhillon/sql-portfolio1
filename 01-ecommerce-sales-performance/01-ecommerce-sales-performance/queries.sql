54d1e5c804c906faf8cc6d9c28312b14f35a1711
sukhman-dhillon <sukhmandhillon185@gmail.com>
2025-12-24 02:23:03 -0700

    queries.sql

---
+++ 01-ecommerce-sales-performance/01-ecommerce-sales-performance/queries.sql
@@ -0,0 +1,23 @@
+-- Project 01: Ecommerce Sales Performance
+-- Dataset: bigquery-public-data.thelook_ecommerce
+
+-- Q1: Monthly Revenue Trend
+-- paste query here
+
+-- Q2: Average Order Value (AOV) by Month
+-- paste query here
+
+-- Q3: Top Products by Revenue
+-- paste query here
+
+-- Q4: Revenue by Category
+-- paste query here
+
+-- Q5: Revenue by Traffic Source
+-- paste query here
+
+-- Q6: Repeat Customer Rate
+-- paste query here
+
+-- Q7: High Value Order Segmentation
+-- paste query here
